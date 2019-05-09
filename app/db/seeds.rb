3980890842eb7e1db1c1fe95e4f73076f7e5c124
Nicolas <nmgrigoriewgridensol@miuandes.cl>
2019-05-09 19:22:39 -0400

    Mast

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -8,18 +8,17 @@
 
 user1 = User.create(name:"Ignacia", lastname:"Urrutia", phone:123456789, email:"miurrutia2@miuandes.cl", password:"ignacia12345")
 user2 = User.create(name:"Nicolas", lastname:"Grigoriew", phone:987654321, email:"nmgrigoriewdensol@miuandes.cl", password:"nicolas12345")
+admin1 = User.create(name:"Benjamin", lastname:"Fuentes", email:"admin@miuandes.cl", password:"hola123456")
+superadmin1 = User.create(name:"Sofia", lastname:"Pavicevic", email:"sadmin@miuandes.cl", password:"hola654321")
 
 post1 = Post.create(user_id:user1.id,title:"car parked wrongly", description:"Stupid owner", city:"Santiago", status:1)
-post2 = Post.create(user_id:user1.id,title:"Things i hate", description:"Stupid people", city:"Santiago", status:1)
+post2 = Post.create(user_id:user2.id,title:"Watch out in parking lot", description:"Hatefull community", city:"Santiago", status:1)
+post2 = Post.create(user_id:user1.id,title:"Things i hate", description:"Stupid people", city:"Concepcion", status:1)
 
-admin1 = Admin.create(name:"1", lastname:"1", email:"hola1@gmail.com", password:"hola123456", geofence:"Santiago")
 
 comment1 = Comment.create(user_id:user2.id, post_id:post1.id)
 
-profile1 = User_profile.create(user_id:user1.id, bio:" ", personal_picture:"photo1", city:"Santiago", country:"Chile", gps:"Chile")
-profile2 = User_profile.create(user_id:user2.id, bio:" ", personal_picture:"photo2", city:"Santiago", country:"Chile", gps:"Chile")
 
-superadmin1 = Superadmin.create(name:"2", lastname:"2", email:"hola2@gmail.com", password:"hola654321", geofence:"Santiago")
 
 dp1 = Dumpster.create(post_id:post2.id)
 
