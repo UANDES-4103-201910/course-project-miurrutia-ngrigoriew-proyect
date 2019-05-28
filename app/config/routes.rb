6dd431b5afb7438193c01c5953b1ca2f75007b70
Ignacia Urrutia <miurrutia2@miuandes.cl>
2019-05-28 11:42:28 -0400

    searchbyname

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -41,5 +41,5 @@ Rails.application.routes.draw do
   resources :blacklist
   get 'blacklist/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get '/search' => 'pages#search', :as => 'search_page'
 end
