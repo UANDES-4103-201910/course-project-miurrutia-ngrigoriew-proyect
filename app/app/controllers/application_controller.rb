aee3171c03e8e541aede04518a5b81bcc4277e8a
Nicolas <nmgrigoriewgridensol@miuandes.cl>
2019-05-30 15:31:08 -0400

    App controller

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 	protected
 
 		def configure_permitted_parameters
-			devise_parameter_sanitizer.permit :sign_up, keys: [:name , :lastname]
+			devise_parameter_sanitizer.permit :sign_up, keys: [:name , :lastname , :city]
 			devise_parameter_sanitizer.permit(:account_update) do |u|
 				u.permit(:name, :email, :city, :password, :password_confirmation, :current_password)
 			end
