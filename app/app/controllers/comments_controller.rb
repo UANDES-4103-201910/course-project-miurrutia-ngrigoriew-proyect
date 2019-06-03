33c50c6be464a1376770afc7cedc8d1c11b2f63b
Nicolas <nmgrigoriewgridensol@miuandes.cl>
2019-06-02 20:42:29 -0400

    Comments casi

--- app/app/controllers/comments_controller.rb
+++ app/app/controllers/comments_controller.rb
@@ -12,7 +12,15 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
     flash[:error]='Comment deleted successfully'
   end
- 
+  
+  def update
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.update(comment_params)
+      redirect_to @post
+      flash[:notice]='Comment updated successfully'
+
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:body).merge(user_id: current_user.id)
