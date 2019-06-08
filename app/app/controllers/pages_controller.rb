28bce41c4c842c88991df979958a31026e00b3b3
Ignacia Urrutia <miurrutia2@miuandes.cl>
2019-06-08 18:24:09 -0400

    SearchComplete

--- app/app/controllers/pages_controller.rb
+++ app/app/controllers/pages_controller.rb
@@ -5,10 +5,11 @@ class PagesController < ApplicationController
     		redirect_to(root_path, alert: "Empty field!") and return  
   		else  
    			@parameter = params[:search].downcase  
-    		@results = User.all.where("lower(name) LIKE :search", search: @parameter) 
-    		@locations = User.all.where("lower(city) LIKE :search", search: @parameter) 
-    		@posttitles = Post.all.where(" lower(title) LIKE (?)", "%#{params[:search]}%")
-    		@postdescriptions = Post.all.where(" lower(description) LIKE (?)", "%#{params[:search]}%")
+    		@results = User.all.where("lower(name) LIKE (?)", "%#{params[:search]}%") 
+    		@locations = User.all.where("lower(city) LIKE (?)", "%#{params[:search]}%") 
+    		@posttitles = Post.all.where("lower(title) LIKE (?)", "%#{params[:search]}%")
+    		@postdescriptions = Post.all.where("lower(description) LIKE (?)", "%#{params[:search]}%")
+    		@authors = Post.all.joins(:user).where("lower(name) LIKE (?)", "%#{params[:search]}%")
 
   		end  
 	end
